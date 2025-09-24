6916ea755dfdb7bedbe1f1a3e73464964ac1a4b9
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-09-24 13:46:54 +0700

    jsx

--- reactpro/src/components/Hello.jsx
+++ reactpro/src/components/Hello.jsx
@@ -1,5 +1,11 @@
+const name = "Marvin";
+
+function displayMessage() {
+  return ("wow, it works!");
+}
+
 function Hello() {
-  return <h1>Hello from React!</h1>
+  return <h1>Hello from React! {displayMessage()}</h1>
 }
 
 export default Hello;
