824087ad2536ceb8adab85bbbadd19c524507e12
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-09-26 12:36:42 +0700

    todo-app: marking todo items as complete

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -4,14 +4,28 @@ export default function TodoItem({ item, todos, setTodos }) {
   function handleDelete(item) {
     console.log("Delete", item);
     setTodos(todos.filter((todo) => todo !== item));
-
   }
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+  const className = item.done ? styles.completed : "";
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item.name}
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
-          <button onClick={() => handleDelete(item)} className={styles.deleteButton}>X</button>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            X
+          </button>
         </span>
       </div>
 
