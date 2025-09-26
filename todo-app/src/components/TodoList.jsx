327f6c3078a73bb8b7812f56d294940424a59997
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-09-26 13:46:54 +0700

    todo-app: sorting item and complete prj

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -1,9 +1,10 @@
 import TodoItem from "./TodoItem";
 import styles from "./todolist.module.css"
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos.slice().sort((a, b) => Number(a.done - b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem item={item} key={item.name} todos={todos} setTodos={setTodos}/>
       ))}
     </div>
