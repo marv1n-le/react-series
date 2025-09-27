f503de866703b1e5c5c738d9850378f601a8640a
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-09-27 16:53:31 +0700

    food-app: styling the food item component

--- food-app/src/components/FoodItem.jsx
+++ food-app/src/components/FoodItem.jsx
@@ -1,7 +1,15 @@
-export default function FoodItem({food}) {
-  return <div>
-    <img src={food.image} alt={food.title} />
-    <h1>{food.title}</h1>
-    <button>View Recipe</button>
-  </div>;
+import styles from "./fooditem.module.css";
+
+export default function FoodItem({ food }) {
+  return (
+    <div className={styles.foodItem}>
+      <img className={styles.itemImage} src={food.image} alt={food.title} />
+      <div className={styles.itemContent}>
+        <p className={styles.itemName}>{food.title}</p>
+      </div>
+      <div className={styles.buttonContainer}>
+        <button className={styles.itemButton}>View Recipe</button>
+      </div>
+    </div>
+  );
 }
