8ca2fe06542d7bd3aa0d4d0c627c409a2d174175
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-09-28 21:59:08 +0700

    useReducer using bank account example

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,50 +1,84 @@
 import { useReducer } from "react";
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-  const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
-  function reducer(state, action) {
-    if (action.type === "increment") {
-      return {
-        ...state,
-        count: state.count + state.incrementBy,
-      };
-      //state + action.payload;
-    } else if (action.type === "decrement") {
-      return {
-        ...state,
-        count: state.count - state.incrementBy,
-      };
-      //state - action.payload
-    }
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [state, dispatch] = useReducer(reducer, { count: 0, incrementBy: 1 });
+//   function reducer(state, action) {
+//     if (action.type === "increment") {
+//       return {
+//         ...state,
+//         count: state.count + state.incrementBy,
+//       };
+//       //state + action.payload;
+//     } else if (action.type === "decrement") {
+//       return {
+//         ...state,
+//         count: state.count - state.incrementBy,
+//       };
+//       //state - action.payload
+//     }
 
-    if (action.type === "setIncrement") {
+//     if (action.type === "setIncrement") {
+//       return {
+//         ...state,
+//         incrementBy: action.payload
+//       }
+//     }
+//   }
+//   return (
+//     <div>
+//       Use Reducer: {state.count}
+//       <input
+//         type="text"
+//         value={state.incrementBy}
+//         onChange={(e) => dispatch({
+//           type: "setIncrement",
+//           payload: Number(e.target.value)
+//         })}
+//       />
+//       <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
+//         Increment count
+//       </button>
+//       <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
+//         Decrement count
+//       </button>
+//     </div>
+//   );
+// }
+
+function App() {
+  function reducer(state, action) {
+    if (action.type === "add") {
       return {
         ...state,
-        incrementBy: action.payload
+        balance: state.balance + action.payload
+      }
+    }
+    if (action.type === "sub") {
+      return {
+        ...state,
+        balance: state.balance - action.payload
       }
     }
   }
-  return (
-    <div>
-      Use Reducer: {state.count}
-      <input
-        type="text"
-        value={state.incrementBy}
-        onChange={(e) => dispatch({
-          type: "setIncrement",
-          payload: Number(e.target.value)
-        })}
-      />
-      <button onClick={() => dispatch({ type: "increment", payload: 1 })}>
-        Increment count
-      </button>
-      <button onClick={() => dispatch({ type: "decrement", payload: 1 })}>
-        Decrement count
-      </button>
-    </div>
-  );
+  const [state, dispatch] = useReducer(reducer, {
+    balance: 0,
+  })
+  const [amount, setAmount] = useState(0);
+
+  return <div>
+    {state.balance}
+    <input type="text" 
+    onChange={(e) => setAmount(Number(e.target.value))}
+    />
+    <button onClick={() =>
+      dispatch({ type: "add", payload: Number(amount)})
+    }>Deposit</button>
+    <button onClick={() => 
+      dispatch({ type: "sub", payload: Number(amount) })
+    }>Widthdraw</button>
+  </div>
 }
 
 export default App;
