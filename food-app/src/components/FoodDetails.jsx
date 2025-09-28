a3ccc94899ac02b39d9f0800fb89dc42938cda29
Le Duc Anh <152796553+marv1n-le@users.noreply.github.com>
2025-09-28 16:53:02 +0700

    food-app: designing the recipe detail component

--- food-app/src/components/FoodDetails.jsx
+++ food-app/src/components/FoodDetails.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
+import styles from './fooddetails.module.css';
 
 export default function FoodDetails({ foodId }) {
   const [food, setFood] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
-  const API_KEY = "cdef266fa396475a9fa61f5cd8ac0d8b";
+  //const API_KEY = "cdef266fa396475a9fa61f5cd8ac0d8b";
+  const API_KEY = "f06e7d20027e4bfdb0dda69aec14b5f5";
+  //const API_KEY = "f75f50fd50904935a36dc5798d9592a6";
+  
 
   useEffect(() => {
     async function fetchFoodDetails() {
@@ -18,28 +22,43 @@ export default function FoodDetails({ foodId }) {
   }, [foodId]);
   return (
     <div>
-      <div>
-        <h1>{food.title}</h1>
-        <img src={food.image} alt={food.title} />
-        <div>
+      <div className={styles.recipeCard}>
+        <h1 className={styles.recipeName}>{food.title}</h1>
+
+        <img className={styles.recipeImage} src={food.image} alt={food.title} />
+        <div className={styles.recipeDetails}>
           <span>
             <strong>⏰ {food.readyInMinutes} minutes</strong>
           </span>
           <span>
             <strong>🧑🏻‍💻 Serves {food.servings}</strong>
           </span>
-          <span>{food.vegeterian ? "🥦 Vegeterian" : "🍖 Non-Vegeterian"}</span>
-          <span>{food.vegan ? "🥗 Vegan" : ""}</span>
+          <span>
+            <strong>
+              {food.vegeterian ? "🥦 Vegeterian" : "🍖 Non-Vegeterian"}
+            </strong>
+          </span>
+          <span>
+            <strong>{food.vegan ? "🥗 Vegan" : ""}</strong>
+          </span>
         </div>
-        <span>💵 {food.pricePerServing / 100} Per serving</span>
-      </div>
-      <div>
+        <div>
+          <span>
+            <strong>💵 {food.pricePerServing / 100} Per serving</strong>
+          </span>
+        </div>
         <h2>Instructions</h2>
-        {isLoading ? (
-          <p>Loading...</p>
-        ) : (
-          food.analyzedInstructions[0].steps.map((step) => <li>{step.step}</li>)
-        )}
+        <div className={styles.recipeInstructions}>
+          <ol>
+            {isLoading ? (
+              <p>Loading...</p>
+            ) : (
+              food.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </ol>
+        </div>
       </div>
     </div>
   );
--- food-app/src/components/Search.jsx
+++ food-app/src/components/Search.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import styles from './search.module.css';
+import styles from "./search.module.css";
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
-const API_KEY = "cdef266fa396475a9fa61f5cd8ac0d8b";
+//const API_KEY = "cdef266fa396475a9fa61f5cd8ac0d8b";
+const API_KEY = "f06e7d20027e4bfdb0dda69aec14b5f5";
 
-export default function Search({foodData, setFoodData}) {
-  const [query, setQuery] = useState('pizza');
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
   //syntax of useEffect hook
   useEffect(() => {
     async function fetchFood() {
@@ -16,12 +17,15 @@ export default function Search({foodData, setFoodData}) {
       setFoodData(data.results);
     }
     fetchFood();
-  }, [query])
-  return <div className={styles.searchContainer}>
-    <input 
-    className={styles.input}
-    value={query}
-    onChange={(e) => setQuery(e.target.value)}
-    type="text"/>
-  </div>
+  }, [query]);
+  return (
+    <div className={styles.searchContainer}>
+      <input
+        className={styles.input}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        type="text"
+      />
+    </div>
+  );
 }
